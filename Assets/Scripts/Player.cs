45c46e21a5770d93fca89e17a699a5d5fd51d36a
rbayuardana <baytech28official@gmail.com>
2021-11-30 16:19:04 +0700

    'LUMOS'

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+
+    Rigidbody2D myRigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Run()
+    {
+        float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        Vector2 playerVel = new Vector2(controlThrow, myRigidBody.velocity.y );
+        myRigidBody.velocity = playerVel;
+    }
+}
